cfc80bc9a4b54b6a57c167592a52b5abe3848a43
ubaid suhail <ubaid.patel50@gmail.com>
2020-10-26 12:55:59 +0500

    Data_Update(26-10-2020)

--- lib/datamodels/apisurl.dart
+++ lib/datamodels/apisurl.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 
-const String checkMobileNumberApi = "https://portalease.com/aimdialapis/consultantaimdialapis/checkmobilenumber.php";
-const String signupApi = "https://portalease.com/aimdialapis/consultantaimdialapis/signup.php";
-const String loginApi = "https://portalease.com/aimdialapis/consultantaimdialapis/login.php";
+const String checkMobileNumberApi = "https://www.portalease.com/aimdialapis/consultantaimdialapis/checkmobilenumber.php";
+const String signupApi = "https://www.portalease.com/aimdialapis/consultantaimdialapis/signup.php";
+const String loginApi = "https://www.portalease.com/aimdialapis/consultantaimdialapis/login.php";
